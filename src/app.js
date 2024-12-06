40aa0957f6c87f6695fbb9ea7bb9d9c15d323119
GautamRamaniAdsum <101443138+GautamRamani@users.noreply.github.com>
2024-12-06 11:48:57 +0530

    add

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(morgan("tiny"))
 
 app.get("/health", (req, res) => {
-    res.status(200).send({ status: "success", msg: "Final Setup" })
+    res.status(200).send({ status: "success", msg: "Commit 1" })
 })
 
 app.use("/api", router)
