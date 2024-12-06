6f60a4f951099044decb1ea0c845986c9cde45d7
GautamRamaniAdsum <101443138+GautamRamani@users.noreply.github.com>
2024-12-06 11:41:09 +0530

    add change

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(morgan("tiny"))
 
 app.get("/health", (req, res) => {
-    res.status(200).send({ status: "success", msg: "CI-CD SETUP" })
+    res.status(200).send({ status: "success", msg: "Final Setup" })
 })
 
 app.use("/api", router)
